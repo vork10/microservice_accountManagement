a08fba1fbaf2c14a8cd68feb3024d37cb8a4ac53
CasperKirud <ckirud@gmail.com>
2024-03-12 09:45:52 +0100

    it now opens browser when running server

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+import webbrowser
 
 # Create Flask application instance
 app = Flask(__name__)
@@ -7,6 +8,9 @@ app = Flask(__name__)
 # Specify template and static file paths
 app.template_folder = 'templates'
 app.static_folder = 'static'
+
+# Open browser
+webbrowser.open_new('http://127.0.0.1:5000')
 
 # Import routes from the app package
 from app import routes
