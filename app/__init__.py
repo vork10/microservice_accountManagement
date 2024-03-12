191c611288e09e960ca94924210e501d94c7499e
vork10 <90039672+vork10@users.noreply.github.com>
2024-03-12 16:14:24 +0100

    start on database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-import os
 from flask import Flask
 import webbrowser
+import firebase_admin
+from firebase_admin import credentials, initialize_app, firestore
 
 # Create Flask application instance
 app = Flask(__name__)
@@ -8,6 +9,16 @@ app = Flask(__name__)
 # Specify template and static file paths
 app.template_folder = 'templates'
 app.static_folder = 'static'
+
+try:
+    # Initialize Firebase Admin SDK
+    cred = credentials.Certificate("erdyssa-default-rtdb-export.json")
+    firebase_app = initialize_app(cred)
+    # Get a reference to the Firestore database
+    db_ref = firestore.client()
+    print("Firebase initialized successfully")
+except Exception as e:
+    print("Firebase initialization failed:", e)
 
 # Open browser
 webbrowser.open_new('http://127.0.0.1:5000')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import render_template, request, redirect, url_for
 from app import app
+import firebase_admin
+from firebase_admin import auth, credentials
 
-print("Loading routes.py")
+
+cred = credentials.Certificate("erdyssa-default-rtdb-export.json")
+firebase_app = firebase_admin.initialize_app(cred)
 
 @app.route('/')
 def login_page():
@@ -21,15 +24,31 @@ def reset_password():
     # Render the password reset page
     return render_template('resetpassword.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Render the registration page
-    return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        
+        if password != confirm_password:
+            return "Passwords do not match"
+        
+        try:
+            user = auth.create_user(
+                email=email,
+                password=password
+            )
+            # Optionally, store additional user data in Firestore
+            # firestore.collection('users').document(user.uid).set({ 'email': email })
+            
+            return redirect(url_for('login_page'))
+        except firebase_admin.auth.AuthError as e:
+            return "Error: " + str(e)
+    else:
+        return render_template('register.html')
 
 @app.route('/dashboard')
 def dashboard():
     # Render the dashboard page
-    return "Dashboard Page"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('dashboard.html')
