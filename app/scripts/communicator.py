1186961908ce96361c5666b86c0f234cbdcb6eee
CasperKirud <ckirud@gmail.com>
2024-06-11 11:05:31 +0200

    more cleanup

--- app/scripts/communicator.py
+++ app/scripts/communicator.py
@@ -2,15 +2,14 @@ import requests
 
 class Communicator:
     def __init__(self):
-        # Initialize session, set base URL or headers if needed
         self.session = requests.Session()
-        self.session.verify = False  # Disable SSL certificate verification
+        self.session.verify = False
 
     def get_data(self, url):
         """Get data from the specified URL."""
         try:
             response = self.session.get(url)
-            response.raise_for_status()  # Raises an HTTPError for bad responses
+            response.raise_for_status()
             return response.text
         except requests.RequestException as e:
             print(f"An error occurred: {e}")
@@ -20,7 +19,7 @@ class Communicator:
         """Post JSON data to the specified URL."""
         try:
             response = self.session.post(url, json=data)
-            response.raise_for_status()  # Raises an HTTPError for bad responses
+            response.raise_for_status()
             return response.text
         except requests.RequestException as e:
             print(f"An error occurred: {e}")
